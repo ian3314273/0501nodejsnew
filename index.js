0fa6832594020c636d6a2d8338c3e386e0482199
ian3314273 <ian3314273@gmail.com>
2024-05-01 10:59:39 +0800

    Not finished

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Parser = require('rss-parser')
 
 let parser = new Parser();
-
+let result =
 // Get https://www.cwa.gov.tw/rss/Data/cwa_warning.xml
 
 async function fetchData() {
@@ -9,6 +9,32 @@ async function fetchData() {
     console.log((feed.title));
 
     feed.items.forEach((item=>{
-        console.log(item.title + ' ' + item.link)
+        console.log(item.title + ' ' + item.link + item.content)
     }));
+}
+
+fetchData();
+
+var http = require('http');
+
+http.createServer(async function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("Hello World!<br>");
+    // 載入檔案
+    let text = await getText('https://www.w3schools.com/js/mycar.html');
+    // 取代"img_car.jpg" 為 "https://www.w3schools.com/js/img_car.jpg" text
+    text = text.replace(/img_car.jpg/g, "https://www.w3schools.com/js/img_car.jpg");
+    res.write(text);
+    console.log("http://localhost:8080" + req.url);
+
+    res.end();
+}).listen(8080);
+
+console.log("本機端的網址: http://localhost:8080");
+
+
+async function getText(file) {
+    let response = await fetch(file);
+    let data = await response.text();
+    return data;
 }
